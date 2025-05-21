f421baf32b7991811aed42e6d3281fab1ce4a7f7
kavindisathsarani <dksmanimendra0@gmail.com>
2025-05-21 13:56:01 +0530

    page designed

--- src/view/common/Footer/Footer.tsx
+++ src/view/common/Footer/Footer.tsx
@@ -1,8 +1,44 @@
 import './Footer.css'
 export function Footer() {
     return (
-        <div className="footer">
-            This is Footer!
-        </div>
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-logo">
+                    <p>GlowUp Salon</p>
+                </div>
+
+                <div className="footer-links">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div className="footer-contact">
+                    <h4>Contact Us</h4>
+                    <p>Email: support@glowup.com</p>
+                    <p>Phone: +1 234 567 890</p>
+                </div>
+
+                <div className="footer-social">
+                    <h4>Follow Us</h4>
+                    <div className="social-icons">
+                        <a href="#"><i className="fab fa-facebook-f"></i></a>
+                        <a href="#"><i className="fab fa-instagram"></i></a>
+                        <a href="#"><i className="fab fa-twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer-bottom">
+                <p>© 2025 GlowUp. All rights reserved.</p>
+            </div>
+        </footer>
+        /* <div className="footer">
+             This is Footer!
+         </div>*/
     );
 }
--- src/view/common/Navbar/Navbar.tsx
+++ src/view/common/Navbar/Navbar.tsx
@@ -1,8 +1,18 @@
 import './Navbar.css'
 export function Navbar() {
     return (
-        <div className="navbar">
-            This is Navbar
-        </div>
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="/pngtree-salon-logo-png-image_4004444.png" alt="Logo"/>
+            </div>
+            <ul className="navbar-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        /* <div className="navbar">
+             This is Navbar
+         </div>*/
     );
 }
