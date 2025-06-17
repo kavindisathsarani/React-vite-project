ce341de096d7395f92d6e8ba4dec83215c0b4746
kavindisathsarani <dksmanimendra0@gmail.com>
2025-06-17 19:22:18 +0530

    productSlice done

--- src/slices/productsSlice.ts
+++ src/slices/productsSlice.ts
@@ -1,5 +1,4 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import type {RootState} from "../store/store.ts";
 
 interface ProductState {
     list: [],
@@ -7,16 +6,16 @@ interface ProductState {
 }
 
 const initialState: ProductState = {
-    list:[],
+    list: [],
     error: null
 }
 
 export const getAllProducts = createAsyncThunk(
     'products/getAllProducts',
-        async () => {
-            const response = await fetch('./product-data.json');
-            return  await response.json();
-        }
+    async () => {
+        const response = await fetch('./product-data.json');
+        return await response.json();
+    }
 )
 
 const productsSlice = createSlice({
@@ -25,16 +24,16 @@ const productsSlice = createSlice({
     initialState: initialState,
     reducers: {},
     extraReducers:
-        (builder)=>{
-        builder
-            .addCase(getAllProducts.pending, () => {
-            alert("Products are still loading...");
-        }).addCase(getAllProducts.fulfilled, (state: RootState, action) => {
-            state.list = action.payload;
-        }).addCase(getAllProducts.rejected, (state: RootState, action) => {
-            state.error = action.error.message;
-            alert("Error loading:"+state.error);
-        })
+        (builder) => {
+            builder
+                .addCase(getAllProducts.pending, () => {
+                    alert("Products are still loading...");
+                }).addCase(getAllProducts.fulfilled, (state, action) => {
+                state.list = action.payload;
+            }).addCase(getAllProducts.rejected, (state, action) => {
+                state.error = action.error.message;
+                alert("Error loading:" + state.error);
+            })
         }
 });
 
